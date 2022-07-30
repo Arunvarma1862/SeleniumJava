aa56924b60873b37d824458be624aa2e2347c1f6
Arunvarma1862 <arunbabu120894@gmail.com>
2022-07-30 10:09:34 +0530

    second commit

--- src/test/java/com/test/uploadFiles.java
+++ src/test/java/com/test/uploadFiles.java
@@ -10,25 +10,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import  java.awt.Robot;
+import  java.awt.datatransfer.StringSelection;
+import  java.awt.Toolkit;
+import  java.awt.event.KeyEvent;
+import java.time.Duration;
 
 public class uploadFiles {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws Exception {
 	
 	   WebDriverManager.chromedriver().setup();
 	   WebDriver driver = new ChromeDriver();   
 	   
 	   driver.manage().window().maximize();
 	   
+	   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(11));  
 	   driver.navigate().to("https://www.monsterindia.com/");
 	   
 	   driver.findElement(By.xpath("//a[@title='Click to upload your resume' and @class='btn block resume-btn btn-orange mt20']")).click();
       
-	                                                   //  using send keys
 	   
-	   driver.findElement(By.id("file-upload")).sendKeys("C:\\New folder\\om.txt");
+	 	                                                   //  using send keys
+	   
+	 //  driver.findElement(By.id("file-upload")).sendKeys("C:\\New folder\\om.txt");*/
 	   
 	   
-	                                                    // using robt class methods
+	                                                    // using robot class methods
 	   
 	WebElement button=   driver.findElement(By.id("file-upload"));
 	JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -40,9 +47,30 @@ public class uploadFiles {
 	 * 3.Enter
 	 */
 	
+	Robot rb = new Robot();
+	rb.delay(2000);
 	
-      
-       
-          ;
+	//put path to file in a clipboard
+	
+	StringSelection ss = new StringSelection("C:\\New folder\\om.txt");
+	Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss,null);
+	
+	//Ctrl+v
+	
+	rb.keyPress(KeyEvent.VK_CONTROL); // press on CRTL key
+	rb.keyPress(KeyEvent.VK_V);
+	rb.delay(2000);
+	
+	rb.keyRelease(KeyEvent.VK_CONTROL);
+	rb.keyRelease(KeyEvent.VK_V);
+	rb.delay(2000);
+	
+	// Enter
+	
+	rb.keyPress(KeyEvent.VK_ENTER); // press Enter
+	rb.keyRelease(KeyEvent.VK_ENTER);
+    
+	
+          
    }
 }
